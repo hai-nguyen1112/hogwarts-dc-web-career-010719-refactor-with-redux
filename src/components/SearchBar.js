b64fd2c0d06bc91cb7be25fe2d24fc00d35a9b50
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-06-03 17:54:54 -0400

    edited search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {changeSearchText} from '../redux/actions'
 import {connect} from 'react-redux'
+import {Input} from 'semantic-ui-react'
 
 class SearchBar extends React.Component {
 
@@ -10,14 +11,20 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <div className="ui very large fluid input">
-        <input
-          type="text"
-          name="search"
-          value={this.props.searchText}
-          placeholder="Search By Name..."
-          onChange={e => this.props.handleTextChange(e.target.value)}
-        />
+      <div id="searchbarcontainer">
+        <div id="searchbartitle" style={{fontSize: "20px", color: "pink", fontWeight: "bold"}}>
+          Search:&nbsp;&nbsp;&nbsp;
+        </div>
+        <div id="searchbar">
+          <Input
+            type="text"
+            name="search"
+            value={this.props.searchText}
+            placeholder="By Name..."
+            onChange={e => this.props.handleTextChange(e.target.value)}
+            style={{width: "50%"}}
+          />
+        </div>
       </div>
     )
   }
